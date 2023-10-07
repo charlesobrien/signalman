768ed1b000fe24cf6773c2935140c73865e0d40a
Charles OBrien <charles@cobrien.org>
2023-10-07 11:18:41 -0400

    statement name

--- lib/signalman/query_handler.rb
+++ lib/signalman/query_handler.rb
@@ -21,10 +21,10 @@ module Signalman
       payload[:sql] = event.payload[:sql]
       #payload[:binds] = event.payload[:binds]
       #payload[:type_casted_binds] = event.payload[:type_casted_binds]
-      #payload[:statement_name] = event.payload[:statement_name]
+      payload[:statement_name] = event.payload[:statement_name]
       #payload[:async] = event.payload[:async]
 
-      create_event event.payload.keys()
+      create_event payload
     end
   end
 end
